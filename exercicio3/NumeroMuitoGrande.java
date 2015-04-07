83cc117e831898468525a98070fc709212f6c327
Bruno Oliveira <saidakiza@gmail.com>
2015-04-06 22:20:39 -0300

    Update NumeroMuitoGrande.java

--- exercicio3/NumeroMuitoGrande.java
+++ exercicio3/NumeroMuitoGrande.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 public class NumeroMuitoGrande {
 
     int flag = 0;
-    int num1[] = new int[30];
-    int num2[] = new int[30];
+    int n1[] = new int[30];
+    int n2[] = new int[30];
     int soma[] = new int[30];
-    int mult[] = new int[30];
 
-
-    public void somar() {
+    public void Soma() {
         int cont = 0;
         for (int i = 29; i >= 0; i--) {
             if (i == 0) {
-                if ((soma[i] + num1[i] + num2[i]) > 10) {
+                if ((soma[i] + n1[i] + n2[i]) > 10) {
                     System.out.println("Numeros grandes demais.");
                     flag = 1;
                     break;
                 }
             }
-            if (soma[i] + num1[i] + num2[i] >= 10) {
-                soma[i] = (soma[i] + num1[i] + num2[i]) - 10;
+            if (soma[i] + n1[i] + n2[i] >= 10) {
+                soma[i] = (soma[i] + n1[i] + n2[i]) - 10;
                 cont = 1;
             } else {
-                soma[i] = soma[i] + num1[i] + num2[i];
+                soma[i] = soma[i] + n1[i] + n2[i];
             }
             if (cont == 1) {
                 cont = 0;
@@ -32,11 +30,10 @@ public class NumeroMuitoGrande {
         }
     }
 
-    
     public void EscreverSoma() {
         int cont = 0;
         System.out.println("Soma: ");
-        
+
         for (int i = 0; i <= 29; i++) {
             if (soma[i] == 0 && cont == 0) {
                 if (soma[i + 1] != 0) {
@@ -47,34 +44,34 @@ public class NumeroMuitoGrande {
             }
         }
     }
-    
+
     public static void main(String[] args) {
         int contarNum1, contarNum2;
+        String num1, num2;
         Scanner input = new Scanner(System.in);
-        NumeroMuitoGrande NM = new NumeroMuitoGrande();
-        String nume1, nume2;
+        NumeroMuitoGrande BigInt = new NumeroMuitoGrande();
 
         System.out.println("1o Numero: ");
-        nume1 = input.next();
+        num1 = input.next();
         System.out.println("2o Numero: ");
-        nume2 = input.next();
-        contarNum1 = nume1.length();
-        contarNum2 = nume2.length();
-        
+        num2 = input.next();
+        contarNum1 = num1.length();
+        contarNum2 = num2.length();
+
         int i, j;
         for (i = contarNum1, j = 0; i > 0; i--, j++) {
-            char caracter = nume1.charAt(j);
-            NM.num1[30 - i] = Character.getNumericValue(caracter);
+            char caracter = num1.charAt(j);
+            BigInt.n1[30 - i] = Character.getNumericValue(caracter);
         }
         for (i = contarNum2, j = 0; i > 0; i--, j++) {
-            char caracter2 = nume2.charAt(j);
-            NM.num2[30 - i] = Character.getNumericValue(caracter2);
+            char caracter2 = num2.charAt(j);
+            BigInt.n2[30 - i] = Character.getNumericValue(caracter2);
         }
-  
-        NM.somar();
-  
-        if (NM.flag != 1) {
-            NM.EscreverSoma();
+
+        BigInt.Soma();
+
+        if (BigInt.flag != 1) {
+            BigInt.EscreverSoma();
         }
-      }
+    }
 }
